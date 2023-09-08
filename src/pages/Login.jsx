8b146627668621a8f43803dc5a28fdf250985980
beckalee7774 <44849338+beckalee7774@users.noreply.github.com>
2023-09-08 13:05:32 -0700

    added login info to login page for potential employers

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,15 @@ import Heading from "../ui/Heading";
 
 function Login() {
   return (
-    <div className="p-3">
+    <div className="p-3 relative">
       <Heading title="LOGIN" emoji="📝" />
       <LoginForm />
+      <div className="absolute left-0 bottom-[-80px]">
+        <span>
+          login with username: beckalee7774 password: passwordtest to test out
+          the application if you do not want to sign up
+        </span>
+      </div>
     </div>
   );
 }
